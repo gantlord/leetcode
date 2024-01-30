b0b6fff82928a8cedeece4794e2cdc6e54953ab7
Robin Bruce <robin.bruce@gmail.com>
2024-01-30 20:48:30 +0100

    copilot generated tests

--- golang/lc2/lc2_test.go
+++ golang/lc2/lc2_test.go
@@ -15,7 +15,30 @@ func Test_twoSum(t *testing.T) {
 		args args
 		want []int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Example 1",
+			args: args{
+				nums:   []int{2, 7, 11, 15},
+				target: 9,
+			},
+			want: []int{0, 1},
+		},
+		{
+			name: "Example 2",
+			args: args{
+				nums:   []int{3, 2, 4},
+				target: 6,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "Example 3",
+			args: args{
+				nums:   []int{3, 3},
+				target: 6,
+			},
+			want: []int{0, 1},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
