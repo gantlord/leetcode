199661f4a6b07e93ddb941add48e9191edeeffe1
Robin Bruce <robin.bruce@gmail.com>
2024-02-01 16:36:24 +0100

    lc222

---
+++ golang/lc222/lc222_test.go
@@ -0,0 +1,25 @@
+package lc222
+
+import "testing"
+
+func Test_countNodes(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{&TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}}}, 3},
+		{"2", args{&TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 5}}, Right: &TreeNode{Val: 3}}}, 5},
+		{"3", args{&TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 5}}, Right: &TreeNode{Val: 3, Left: &TreeNode{Val: 6}}}}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countNodes(tt.args.root); got != tt.want {
+				t.Errorf("countNodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
