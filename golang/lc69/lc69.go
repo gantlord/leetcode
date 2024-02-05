b6f47dbd3e4a9f207cdc3efb9d2e40dff5444e83
Robin Bruce <robin.bruce@gmail.com>
2024-02-05 11:16:03 +0100

    improved lc69

--- golang/lc69/lc69.go
+++ golang/lc69/lc69.go
@@ -4,18 +4,11 @@ func mySqrt(x int) int {
 	l, r := 0, 1<<16
 	for l < r {
 		m := l + (r-l)/2
-		s := m * m
-		if s == x {
-			return m
-		}
-		if s > x {
-			r = m - 1
+		if m*m > x {
+			r = m
 		} else {
 			l = m + 1
 		}
 	}
-	if l*l > x {
-		l--
-	}
-	return l
+	return l - 1
 }
