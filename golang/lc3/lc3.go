a0da8916506f26ae1a324eb208c690b44c7d7e9d
Robin Bruce <robin.bruce@gmail.com>
2024-02-21 21:01:45 +0100

    lc3

--- golang/lc3/lc3.go
+++ golang/lc3/lc3.go
@@ -1,20 +1,15 @@
 package lc3
 
 func lengthOfLongestSubstring(s string) int {
-	maxLen := 0
-	for i := 0; i < len(s); i++ {
-		seen := map[byte]int{}
-		l := 0
-		for j := i; j < len(s); j++ {
-			if index, exists := seen[s[j]]; exists {
-				i = index
-				break
-			} else {
-				seen[s[j]] = j
-				l++
-				maxLen = max(l, maxLen)
-			}
-
+	seen := map[rune]int{}
+	maxLen, start := 0, 0
+	for i, c := range s {
+		if index, exists := seen[c]; exists && index >= start {
+			start = index + 1
+		}
+		seen[c] = i
+		if l := i - start + 1; l > maxLen {
+			maxLen = l
 		}
 	}
 	return maxLen
---
+++ golang/lc3/lc3_test.go
@@ -0,0 +1,24 @@
+package lc3
+
+import "testing"
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case1", args{"dvdf"}, 3},
+		{"case2", args{"abcabcbb"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
