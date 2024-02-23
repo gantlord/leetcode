78182e86627694a5477b9fb5cc3bd1a01a6c30d0
Robin Bruce <robin.bruce@gmail.com>
2024-02-24 00:14:57 +0100

    lc1720

---
+++ golang/lc1720/lc1720.go
@@ -0,0 +1,10 @@
+package lc1720
+
+func decode(encoded []int, first int) []int {
+	result := make([]int, len(encoded)+1)
+	result[0] = first
+	for i := 1; i < len(result); i++ {
+		result[i] = result[i-1] ^ encoded[i-1]
+	}
+	return result
+}
