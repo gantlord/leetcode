0be6f6deb6700f6f4449391778cda73962d733b6
Robin Bruce <robin.bruce@gmail.com>
2024-02-23 13:06:35 +0100

    lc452

--- golang/lc452/lc452_test.go
+++ golang/lc452/lc452_test.go
@@ -24,3 +24,23 @@ func Test_merge(t *testing.T) {
 		})
 	}
 }
+
+func Test_findMinArrowShots(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMinArrowShots(tt.args.points); got != tt.want {
+				t.Errorf("findMinArrowShots() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
