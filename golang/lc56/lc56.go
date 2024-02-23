9acb5bf3c96a391a8cf27b231400836809a04322
Robin Bruce <robin.bruce@gmail.com>
2024-02-23 12:38:10 +0100

    lc56

---
+++ golang/lc56/lc56.go
@@ -0,0 +1,21 @@
+package lc56
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	candidate := intervals[0]
+	result := [][]int{}
+	for i := 1; i < len(intervals); i++ {
+		if candidate[1] >= intervals[i][0] {
+			candidate = []int{candidate[0], max(intervals[i][1], candidate[1])}
+		} else {
+			result = append(result, candidate)
+			candidate = intervals[i]
+		}
+	}
+	result = append(result, candidate)
+	return result
+}
