fe7ebf7deb3fc02ba6992226bb051dfb24b7b440
Robin Bruce <robin.bruce@gmail.com>
2024-02-23 19:08:41 +0100

    lc138

---
+++ golang/lc138/lc138.go
@@ -0,0 +1,32 @@
+package lc138
+
+func copyRandomList(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	oldToNew := map[*Node]*Node{}
+	ptr := head
+	newHead := &Node{}
+	newPtr := newHead
+
+	for {
+		newPtr.Val = ptr.Val
+		oldToNew[ptr] = newPtr
+		if ptr.Next == nil {
+			break
+		}
+		ptr = ptr.Next
+		newPtr.Next = &Node{}
+		newPtr = newPtr.Next
+	}
+	ptr = head
+	newPtr = newHead
+	for ptr != nil {
+		if ptr.Random != nil {
+			newPtr.Random = oldToNew[ptr.Random]
+		}
+		ptr = ptr.Next
+		newPtr = newPtr.Next
+	}
+	return newHead
+}
