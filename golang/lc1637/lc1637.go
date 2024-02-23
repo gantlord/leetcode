da9899bbcd8b393a27c76315885068858ca0bad5
Robin Bruce <robin.bruce@gmail.com>
2024-02-23 22:31:27 +0100

    lc1637

---
+++ golang/lc1637/lc1637.go
@@ -0,0 +1,14 @@
+package lc1637
+
+import "sort"
+
+func maxWidthOfVerticalArea(points [][]int) int {
+	sort.Slice(points, func(i, j int) bool {
+		return points[i][0] < points[j][0]
+	})
+	maxWidth := 0
+	for i := 1; i < len(points); i++ {
+		maxWidth = max(maxWidth, points[i][0]-points[i-1][0])
+	}
+	return maxWidth
+}
---
+++ golang/lc1637/lc1637_test.go
@@ -0,0 +1,23 @@
+package lc1637
+
+import "testing"
+
+func Test_maxWidthOfVerticalArea(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxWidthOfVerticalArea(tt.args.points); got != tt.want {
+				t.Errorf("maxWidthOfVerticalArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
