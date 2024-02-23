400568b24cb00b92bfe37520b898377a6e104176
Robin Bruce <robin.bruce@gmail.com>
2024-02-23 22:06:28 +0100

    lc1470

---
+++ golang/lc1470/lc1470.go
@@ -0,0 +1,10 @@
+package lc1470
+
+func shuffle(nums []int, n int) []int {
+	result := make([]int, 2*n)
+	for i := 0; i < n; i++ {
+		result[2*i] = nums[i]
+		result[2*i+1] = nums[i+n]
+	}
+	return result
+}
