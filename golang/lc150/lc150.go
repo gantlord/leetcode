28b9d8e346faa36fb8f424a726d7b120628b0348
Robin Bruce <robin.bruce@gmail.com>
2024-02-23 18:17:11 +0100

    lc150 more efficient

--- golang/lc150/lc150.go
+++ golang/lc150/lc150.go
@@ -2,105 +2,47 @@ package lc150
 
 import "strconv"
 
-type OpType int
-
-const (
-	NIL OpType = iota
-	MUL
-	DIV
-	ADD
-	SUB
-	RES
-)
-
-type StackElem struct {
-	a, b      *int64
-	operation OpType
-}
-
 type stackType struct {
-	array []StackElem
+	array []int
 }
 
 func NewStack() *stackType {
-	s := stackType{[]StackElem{}}
+	s := stackType{[]int{}}
 	return &s
 }
 
-func (s *stackType) Pop() StackElem {
+func (s *stackType) Pop() int {
 	lastIndex := len(s.array) - 1
 	r := s.array[lastIndex]
 	s.array = s.array[:lastIndex]
 	return r
 }
 
-func (s *stackType) Push(r StackElem) {
+func (s *stackType) Push(r int) {
 	s.array = append(s.array, r)
 }
 
-func (s *stackType) Len() int {
-	return len(s.array)
-}
-
-func (s *stackType) GetArray() []StackElem {
-	return s.array
-}
-
 func evalRPN(tokens []string) int {
-	elem := StackElem{}
 	s := NewStack()
 
-	for i := len(tokens) - 1; i >= 0; i-- {
-		var operation OpType = NIL
-		val := new(int64)
-		switch tokens[i] {
-		case "*":
-			operation = MUL
-		case "/":
-			operation = DIV
+	for _, token := range tokens {
+		switch token {
 		case "+":
-			operation = ADD
+			s.Push(s.Pop() + s.Pop())
 		case "-":
-			operation = SUB
+			s.Push(-s.Pop() + s.Pop())
+		case "*":
+			s.Push(s.Pop() * s.Pop())
+		case "/":
+			b := s.Pop()
+			a := s.Pop()
+			s.Push(a / b)
 		default:
-			*val, _ = strconv.ParseInt(tokens[i], 10, 64)
-		}
-
-		elem = processElem(s, elem, operation, val)
-
-	}
-	return int(*elem.a)
-}
-
-func processElem(s *stackType, elem StackElem, operation OpType, val *int64) StackElem {
-	if elem.operation != NIL && operation != NIL {
-		s.Push(elem)
-		elem = StackElem{}
-		elem.operation = operation
-	} else if operation != NIL {
-		elem.operation = operation
-	} else {
-		if elem.a == nil {
-			elem.a = val
-		} else if elem.b == nil {
-			elem.b = val
-			var result int64 = 0
-			switch elem.operation {
-			case MUL:
-				result = *elem.b * *elem.a
-			case DIV:
-				result = *elem.b / *elem.a
-			case ADD:
-				result = *elem.b + *elem.a
-			case SUB:
-				result = *elem.b - *elem.a
-			}
-			if s.Len() == 0 {
-				return StackElem{&result, nil, RES}
-			}
-			next := s.Pop()
-			elem = processElem(s, next, NIL, &result)
+			v, _ := strconv.Atoi(token)
+			s.Push(v)
 		}
 	}
-	return elem
+
+	return s.Pop()
+
 }
