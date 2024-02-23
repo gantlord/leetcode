426f14c1c851c68c84f5bb996e6193e966ebb5e6
Robin Bruce <robin.bruce@gmail.com>
2024-02-23 22:43:58 +0100

    lc2235

---
+++ golang/lc2235/lc2235_test.go
@@ -0,0 +1,24 @@
+package lc2235
+
+import "testing"
+
+func Test_sum(t *testing.T) {
+	type args struct {
+		num1 int
+		num2 int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sum(tt.args.num1, tt.args.num2); got != tt.want {
+				t.Errorf("sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
