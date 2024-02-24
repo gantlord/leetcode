5abfe9146f59e91e8f0c7c87588e80b3af3d7924
Robin Bruce <robin.bruce@gmail.com>
2024-02-24 20:52:55 +0100

    simpler code by allocating first node

--- golang/lc86/lc86.go
+++ golang/lc86/lc86.go
@@ -9,35 +9,21 @@ func partition(head *ListNode, x int) *ListNode {
 	if head == nil {
 		return nil
 	}
-	var headLT, headGTE *ListNode
+	headLT, headGTE := &ListNode{}, &ListNode{}
 	ptrLT, ptrGTE := headLT, headGTE
 	ptr := head
 	for ptr != nil {
 		next := ptr.Next
 		if ptr.Val < x {
-			if headLT == nil {
-				ptrLT = ptr
-				headLT = ptr
-			} else {
-				ptrLT.Next = ptr
-				ptrLT = ptr
-			}
+			ptrLT.Next = ptr
+			ptrLT = ptr
 		} else {
-			if headGTE == nil {
-				ptrGTE = ptr
-				headGTE = ptr
-			} else {
-				ptrGTE.Next = ptr
-				ptrGTE = ptr
-			}
+			ptrGTE.Next = ptr
+			ptrGTE = ptr
 			ptrGTE.Next = nil
 		}
 		ptr = next
 	}
-	if headLT == nil {
-		return headGTE
-	} else {
-		ptrLT.Next = headGTE
-	}
-	return headLT
+	ptrLT.Next = headGTE.Next
+	return headLT.Next
 }
