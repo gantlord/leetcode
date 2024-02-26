9f74f4dffb09a140941540d38b807805c8e44ae4
Robin Bruce <robin.bruce@gmail.com>
2024-02-26 17:24:48 +0100

    lc1486

---
+++ golang/lc1486/lc1486_test.go
@@ -0,0 +1,24 @@
+package lc1486
+
+import "testing"
+
+func Test_xorOperation(t *testing.T) {
+	type args struct {
+		n     int
+		start int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := xorOperation(tt.args.n, tt.args.start); got != tt.want {
+				t.Errorf("xorOperation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
