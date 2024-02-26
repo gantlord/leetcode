979e12ff1d67a92099bfc32f96ad0ca43a9e0390
Robin Bruce <robin.bruce@gmail.com>
2024-02-26 20:35:40 +0100

    lc709

---
+++ golang/lc709/lc709_test.go
@@ -0,0 +1,23 @@
+package lc709
+
+import "testing"
+
+func Test_toLowerCase(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toLowerCase(tt.args.s); got != tt.want {
+				t.Errorf("toLowerCase() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
