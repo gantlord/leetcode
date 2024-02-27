694ba7f56ebfe437f35ef4ddabc15c7f8f9bcd60
Robin Bruce <robin.bruce@gmail.com>
2024-02-27 11:50:14 +0100

    lc2367

---
+++ golang/lc2367/lc2367_test.go
@@ -0,0 +1,24 @@
+package lc2367
+
+import "testing"
+
+func Test_arithmeticTriplets(t *testing.T) {
+	type args struct {
+		nums []int
+		diff int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := arithmeticTriplets(tt.args.nums, tt.args.diff); got != tt.want {
+				t.Errorf("arithmeticTriplets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
