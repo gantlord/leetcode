d4330f6ca0aaca8131e20e2e9c6af8a8628fd5e5
Robin Bruce <robin.bruce@gmail.com>
2024-02-27 15:15:09 +0100

    lc1534

---
+++ golang/lc1534/lc1534_test.go
@@ -0,0 +1,26 @@
+package lc1534
+
+import "testing"
+
+func Test_countGoodTriplets(t *testing.T) {
+	type args struct {
+		arr []int
+		a   int
+		b   int
+		c   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countGoodTriplets(tt.args.arr, tt.args.a, tt.args.b, tt.args.c); got != tt.want {
+				t.Errorf("countGoodTriplets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
