7839f6f6bbfcd2cd170ecf625b4c7b00993c39a2
Robin Bruce <robin.bruce@gmail.com>
2024-02-28 22:01:00 +0100

    lc617

---
+++ golang/lc617/lc617_test.go
@@ -0,0 +1,27 @@
+package lc617
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mergeTrees(t *testing.T) {
+	type args struct {
+		root1 *TreeNode
+		root2 *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTrees(tt.args.root1, tt.args.root2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeTrees() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
