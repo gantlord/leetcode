117a8f547ad93472d5826579f351ac3f274113ca
Robin Bruce <robin.bruce@gmail.com>
2024-02-28 23:15:55 +0100

    lc1704 test case

--- golang/lc1704/lc1704_test.go
+++ golang/lc1704/lc1704_test.go
@@ -11,7 +11,7 @@ func Test_halvesAreAlike(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{"case1", args{"book"}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
