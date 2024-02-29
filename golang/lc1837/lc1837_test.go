a0bc886dae19ffda994725cf6c282a971629cff5
Robin Bruce <robin.bruce@gmail.com>
2024-02-29 13:53:51 +0100

    lc1837

---
+++ golang/lc1837/lc1837_test.go
@@ -0,0 +1,24 @@
+package lc1837
+
+import "testing"
+
+func Test_sumBase(t *testing.T) {
+	type args struct {
+		n int
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumBase(tt.args.n, tt.args.k); got != tt.want {
+				t.Errorf("sumBase() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
