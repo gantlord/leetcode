7603282fc3220e2d7fd88266db6a65aaeff7dcf0
Robin Bruce <robin.bruce@gmail.com>
2024-02-29 12:02:13 +0100

    lc2185

---
+++ golang/lc2185/lc2185_test.go
@@ -0,0 +1,24 @@
+package lc2185
+
+import "testing"
+
+func Test_prefixCount(t *testing.T) {
+	type args struct {
+		words []string
+		pref  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := prefixCount(tt.args.words, tt.args.pref); got != tt.want {
+				t.Errorf("prefixCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
