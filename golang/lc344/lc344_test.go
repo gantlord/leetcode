97293dd87e3e4eda9db8b12bd676a54d0ec22e1b
Robin Bruce <robin.bruce@gmail.com>
2024-02-29 12:52:12 +0100

    lc344

---
+++ golang/lc344/lc344_test.go
@@ -0,0 +1,20 @@
+package lc344
+
+import "testing"
+
+func Test_reverseString(t *testing.T) {
+	type args struct {
+		s []byte
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reverseString(tt.args.s)
+		})
+	}
+}
